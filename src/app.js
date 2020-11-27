ee590e971e8c154d8c2e5eefc5c47d3ca6b20b35
kate2513 <katy.s.golovina@gmail.com>
2020-11-27 11:39:16 +0300

    blank project added

---
+++ src/app.js
@@ -0,0 +1,39 @@
+//const ol = require('ol');
+const express = require('express')
+const app = express();
+app.use('/public', express.static('public'));
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+//adding to mongodb example
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("userdb");
+    var myobj = {content: "Hello world", purpose: "start"};
+    dbo.collection("greetings").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        console.log(res.ops);
+        db.close();
+    });
+});
+
+app.get("/", (req,res) => {
+    //getting from mongodb example
+    let resultArray = [];
+    console.log("on the main page");
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("userdb");
+        dbo.collection("greetings").find({}, { projection: { _id: 0, purpose: 0} }).toArray(function(err, result) {
+            if (err) throw err;
+            res.render("index", {title: result[0].content})
+            db.close();
+        });
+    });
+});
+
+app.listen(3000);
